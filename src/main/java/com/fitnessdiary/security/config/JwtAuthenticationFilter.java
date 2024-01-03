cae531572c1053d1833ae25fa67f2eea0c0c345d
Quinton <149415184+quinton-k@users.noreply.github.com>
2024-01-03 14:04:31 -0700

    Initial Commit

---
+++ src/main/java/com/fitnessdiary/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.fitnessdiary.security.config;
+
+import com.fitnessdiary.service.auth.JWTService;
+import com.fitnessdiary.service.user.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader,"Bearer ")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+
+        if (StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+
+            if (jwtService.isTokenValid(jwt,userDetails)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                        userDetails,null,userDetails.getAuthorities()
+                );
+
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                context.setAuthentication(token);
+
+                SecurityContextHolder.setContext(context);
+            }
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+    private final JWTService jwtService;
+    private final UserService userService;
+}
